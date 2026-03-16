ab79c010f344d7d14a9c61d67f8f0f03d3c26b6a
pixelator-coder <imaginary@fake.com>
2026-03-16 19:51:39 -0400

    Prints percentage of each character

--- frequency_analysis.py
+++ frequency_analysis.py
@@ -1,7 +1,10 @@
 ciphertext = "pm ol ohk hufaopun jvumpkluaphs av zhf, ol dyval pa pu jpwoly, aoha pz, if zv johunpun aol vykly vm aol slaalyz vm aol hswohila, aoha uva h dvyk jvbsk il thkl vba."
 char_count = {}
+total_count = 0
 for character in ciphertext:
     char_count[character] = char_count.get(character, 0) + 1
+    total_count += 1
 sorted_dict = dict(sorted(char_count.items()))
 for character, count in sorted_dict.items():
-    print(character + f"{count:>5}")
+    percentage = round(count*100/total_count, 1)
+    print(character + f"{count:>5}"+ f"{percentage:>7}")
