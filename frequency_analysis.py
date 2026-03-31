9d4a5fab2574c73476661b89ef56372982c5eaaf
pixelator-coder <imaginary@fake.com>
2026-03-30 20:16:50 -0400

    Prints how many letters the first, second, and third most common letters neighbor

--- frequency_analysis.py
+++ frequency_analysis.py
@@ -7,9 +7,10 @@ for character in ciphertext:
     char_count[character] = char_count.get(character, 0) + 1
     total_count += 1
 sorted_dict = dict(sorted(char_count.items(), key=lambda item: item[1], reverse=True))
-most_common_letter = list(sorted_dict.keys())[0]
-second_most_common_letter = list(sorted_dict.keys())[1]
-third_most_common_letter = list(sorted_dict.keys())[2]
+letters_sorted_by_count = list(sorted_dict.keys())
+most_common_letter = letters_sorted_by_count[0]
+second_most_common_letter = letters_sorted_by_count[1]
+third_most_common_letter = letters_sorted_by_count[2]
 print("Most common letter: " + most_common_letter)
 print("Second most common letter: " + second_most_common_letter)
 print("Third most common letter: " + third_most_common_letter)
@@ -25,3 +26,27 @@ for ord in range(97,122,1):
     else:
         print("no")
 print("The most common letter neighbors " + str(num_neighboring_letters) + " letters.")
+
+num_neighboring_letters = 0
+for ord in range(97,122,1):
+    if second_most_common_letter + chr(ord) in ciphertext:
+        print("yes " + second_most_common_letter + chr(ord))
+        num_neighboring_letters += 1
+    elif chr(ord) + second_most_common_letter in ciphertext:
+        print("yes " + chr(ord) + second_most_common_letter)
+        num_neighboring_letters += 1
+    else:
+        print("no")
+print("The 2nd most common letter neighbors " + str(num_neighboring_letters) + " letters.")
+
+num_neighboring_letters = 0
+for ord in range(97,122,1):
+    if third_most_common_letter + chr(ord) in ciphertext:
+        print("yes " + third_most_common_letter + chr(ord))
+        num_neighboring_letters += 1
+    elif chr(ord) + third_most_common_letter in ciphertext:
+        print("yes " + chr(ord) + third_most_common_letter)
+        num_neighboring_letters += 1
+    else:
+        print("no")
+print("The 3rd most common letter neighbors " + str(num_neighboring_letters) + " letters.")
